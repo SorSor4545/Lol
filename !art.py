39628cfe5368a7f4d913134e054cd1135b213c19
SorSor4545 <152562596+SorSor4545@users.noreply.github.com>
2023-12-14 16:57:28 -0500

    Add files via upload

---
+++ !art.py
@@ -0,0 +1,19 @@
+userAge = input ("What's your age? : ") 
+ageNumber = int ( userAge )
+
+userResponse = " :p "
+
+if ageNumber > 17 : 
+    while userResponse != "":
+        userName = input ("What's your name? : ") 
+        userPassword = input ("What's your Password? : ") 
+        userResponse = input ("Hit enter to continue : ") 
+    print ("Great job :) ")
+    userPasponse = input ("Hit enter to continue ")
+    print ("WELCOME TO !art !!")
+
+else: 
+   print ("Your to young :( ")  
+
+    
+
